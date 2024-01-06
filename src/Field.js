a736755a167b3a71166dd0bdaaa50dd119ed2268
NomanLodhi <110075584+NomanLodhi@users.noreply.github.com>
2024-01-06 10:53:10 -0800

    Src Folder Changes Done

--- src/Field.js
+++ src/Field.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import Profile from './Profile.jpg'
 export default function Field() {
   return (
   <>
   <div className="container d-flex align-item-center justify-content-center p-5 my-5 ">
     <div className="card bg-card border border-light text-light border-3 py-4 ">
-      <img src="/Profile.jpg" alt="" className='w-25 mx-auto rounded-circle my-4' />
+      <img src={Profile} alt="" className='w-25 mx-auto rounded-circle my-4' />
       <p className="text-warning mx-auto fw-bold display-5">Noman Lodhi</p>
       <div className="rounded-pill btn btn-outline-light w-75 mx-auto my-3 btn-social fw-bold fs-5 "><a href="https://www.instagram.com/nomanlodhi348/" className=' text-decoration-none w-100' target='main'><i class="bi bi-instagram mx-2"></i> Instagram</a></div>
       <div className="rounded-pill btn btn-outline-light w-75 mx-auto my-3 btn-social fw-bold fs-5 "><a href="https://www.facebook.com/noman.lodhi.733/" target='main' className=' text-decoration-none w-100'> <i class="bi bi-facebook mx-2"></i>Facebook</a></div>
