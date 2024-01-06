ffa4616487f11e884f4218104c31642c9e1749c4
NomanLodhi <110075584+NomanLodhi@users.noreply.github.com>
2024-01-06 11:07:18 -0800

    Src Folder Changes Done

--- src/Field.js
+++ src/Field.js
@@ -11,7 +11,7 @@ export default function Field() {
       <div className="rounded-pill btn btn-outline-light w-75 mx-auto my-3 btn-social fw-bold fs-5 "><a href="https://www.facebook.com/noman.lodhi.733/" target='main' className=' text-decoration-none w-100'> <i class="bi bi-facebook mx-2"></i>Facebook</a></div>
       <div className="rounded-pill btn btn-outline-light w-75 mx-auto my-3 btn-social fw-bold fs-5 "><a href="https://www.linkedin.com/in/noman-lodhi-274313247/" target='main' className=' text-decoration-none w-100'><i class="bi bi-linkedin mx-2"></i> LinkedIn</a></div>
       <div className="rounded-pill btn btn-outline-light w-75 mx-auto my-3 btn-social fw-bold fs-5 "><a href="https://www.threads.net/@nomanlodhi348" target='main' className=' text-decoration-none w-100'><i class="bi bi-threads mx-2"></i>Threads</a></div>
-      <div className="rounded-pill btn btn-outline-light w-75 mx-auto my-3 btn-social fw-bold fs-5 "><a href="https://github.com/NomanLodhi" target='main' className=' text-decoration-none w-100'><i class="bi bi-github mx-2"></i>Githib</a></div>
+      <div className="rounded-pill btn btn-outline-light w-75 mx-auto my-3 btn-social fw-bold fs-5 "><a href="https://github.com/NomanLodhi" target='main' className=' text-decoration-none w-100'><i class="bi bi-github mx-2"></i>Github</a></div>
     </div>
   </div>
   </>
